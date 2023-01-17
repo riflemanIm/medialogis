44baf16afcd85593558c7783c9993162590e48e4
Oleg La <oleglambin@gmail.com>
2023-01-17 14:02:51 +0300

    testimonials

--- data/testimonial.js
+++ data/testimonial.js
@@ -3,13 +3,13 @@ module.exports = [
     img: "1",
     desc: "Je confirme l’intérêt évident de Pneumologis dans mon activité quotidienne de pneumologue: simplicité, rapidité et efficacité. Le logiciel est très pratique à utiliser, il n’y a de remarques majeures à faire en comparaison avec d’autres logiciels. Un aspect très IMPORTANT : ce logiciel est totalement paramétrable à sa demande et à ses besoins propres. En toute honnêteté, je n’ai pas de reproche à donner. Nous sommes équipés depuis 3 ans et pourquoi pas jusqu’à la retraite...",
     name: "Docteur Philippe de Tauriac DOUAI",
-    designation: "Pneumologue",
+    designation: "Pneumologis",
     delayAnimation: "",
   },
   {
     img: "2",
     name: "Docteur Marc Dedieu CARPENTRAS",
-    designation: "Pneumologue",
+    designation: "Pneumologis",
     desc: (
       <>
         <p>Parce qu’il est génial!</p>
@@ -33,7 +33,7 @@ module.exports = [
   {
     img: "3",
     name: "Docteur Olivier Gervais PARIS",
-    designation: "de Pneumologue",
+    designation: "Pneumologis",
     desc: (
       <>
         <p>
@@ -52,5 +52,53 @@ module.exports = [
       </>
     ),
     delayAnimation: "200",
+  },
+  {
+    img: "4",
+    name: "Docteur Bernard Le Brozec LIBOURNE",
+    designation: "Pneumologue",
+    desc: (
+      <>
+        <p>
+          Apres de nombreuses années d’utilisation de Pneumologis sans aucun
+          soutien papier, je ne peux que me dire satisfait du logiciel ou bien
+          je passerais pour un pneumologue masochiste. En fait ma satisfaction
+          est tirée de mes expériences (purement professionnelles !) avec les
+          nouvelles secrétaires et deux remplaçantes. Nos jeunes collègues ont
+          été capables de compulser et de compléter un dossier au bout d’une
+          heure d’apprentissage. Certes elles sont jeunes et l’esprit vif, mais
+          cette rapidité d’adaptation est à partager avec la convivialité et
+          l’ergonomie logique du logiciel. Quant aux secrétaires, il leur a
+          fallu une demi-journée pour maitriser les fonctions essentielles :
+          courrier, prise de rendez vous, encaissement et télétransmission. Le
+          confort apporte par PNEUMOLOGIS fait oublie (partiellement) la
+          pénibilité de la profession. Il permet par ailleurs de mieux connaitre
+          l’activité réelle du cabinet. En un clic, le nombre exact de patients
+          consultant pour la première fois durant une période déterminée. Deux
+          clics : la prévalence de la BPCO par rapport à d’autres pathologies,
+          etc... Le Pneumo devient un véritable gestionnaire de son cabinet.
+          Mais pour être Pneumo-logique, il vaut mieux l’essayer...
+        </p>
+        <p></p>
+      </>
+    ),
+    delayAnimation: "300",
+  },
+  {
+    img: "5",
+    name: "Docteur Paul Chevalier AVIGNON",
+    designation: "Pneumologue",
+    desc: (
+      <>
+        <p>
+          Nous utilisons Pneumologis depuis 4 ans. Apres avoir essaye plusieurs
+          logiciels, c’est celui qui correspondait a une très bonne adaptation a
+          notre cahier des charges (largement confirme au cours de l’évolution),
+          avec convivialité ++.
+        </p>
+        <p></p>
+      </>
+    ),
+    delayAnimation: "400",
   },
 ];
