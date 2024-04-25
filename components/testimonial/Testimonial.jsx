099e495d942bd76e8ac5f386061f827c24910277
Oleg La <oleglambin@gmail.com>
2024-04-25 18:04:46 +0300

    fix: remove testimonials

--- components/testimonial/Testimonial.jsx
+++ components/testimonial/Testimonial.jsx
@@ -44,15 +44,16 @@ export default function Testimonial() {
                   <div className="list_inner">
                     <div className="details">
                       <div className="author">
-                        <div className="image">
-                          <div
+                        {/*<div className="image">
+                          
+                         <div
                             className="main"
                             style={{
                               backgroundImage: `url(img/testimonials/testimonial-${val.img}.jpg)`,
                             }}
                           ></div>
                         </div>
-                        {/* End image */}
+                         End image */}
 
                         <div className="short">
                           <h3>{val.name}</h3>
